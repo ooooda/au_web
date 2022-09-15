9ab098d1f2d610d09ca42ab95baac237f9c90de8
ooooda <anya.smirdina@gmail.com>
2022-09-15 14:41:21 +0300

    Remove commented.

--- labs.js
+++ labs.js
@@ -1,12 +1,3 @@
-// const showButtons = document.getElementsByClassName("show-info-button");
-//
-// showButtons.forEach(showButton => showButton.addEventListener("click", function (e) {
-//    const eyeIcon = document.getElementById("eye").getElementsByClassName(showButton.id)[0]
-//    this.classList.toggle("fa-eye-slash");
-//    showButton.setAttribute("value", (showButton.value === "hide") ? "show" : "hide");
-//    })
-// );
-
 function showHide(id) {
     const eye = document.getElementById(id);
     const article = document.getElementById(id+"-article");
